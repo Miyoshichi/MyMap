bc98a5c3ad983689474a9636618e0c87b24b16f2
Miyoshichi <ruri@Proc12.localdomain>
2019-12-02 18:38:26 +0900

    Initialization

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB85tjhl9s8iy04nKQRRh3qswN9h7GXJ4E',
+    libraries: 'places'
+  },
+  installComponents: true
+})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    Google Map
+    <div class="google-map" id="allmap"></div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB85tjhl9s8iy04nKQRRh3qswN9h7GXJ4E&callback=initMap">
+</script>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      map: null,
+      lat: 35.6048718,
+      lng: 139.6738334
+    }
+  },
+  mounted() {
+    this.initMap()
+  },
+  methods: {
+    initMap() {
+      this.map = new google.maps.Map(document,getElementById("allmap"), {
+        center: {lat: this.lat, lng: this.lng},
+        zoom: 16,
+        maxZoom: 18,
+        minZoom: 10
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+.google-map {
+  width: 100%;
+  height: 600px;
+}
+</style>
